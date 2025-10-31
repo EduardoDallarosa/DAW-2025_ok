e7759158fe98be96e905e2a089fa5705d5a39ba3
EduardoDallarosa <eduardodallarosa321@gmail.com>
2025-10-30 21:55:04 -0300

    Aula 30/10/2025

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,12 +1,17 @@
+import { MovieContent } from "./cases/movies/components/movie-content"
 import { Footer } from "./components/layout/footer"
 import { Header } from "./components/layout/header"
+import { NavBar } from "./components/layout/navbar"
 
 function App() {
 
   return (
     <>
       <Header/>
-      <main></main>
+      <main>
+        <NavBar/>
+        <MovieContent/>
+      </main>
       <Footer/>
     </>
   )
